cf386db4798270650b38a801d92fedf1140efa63
felipe.salles <fsalles@detran.ms.gov.br>
2022-12-23 14:13:13 -0400

    Apresentando detalhes do perfil do usuario logado

--- src/main/java/com/projeto/controller/PerfilController.java
+++ src/main/java/com/projeto/controller/PerfilController.java
@@ -1,17 +1,28 @@
 package com.projeto.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.projeto.model.Usuario;
+import com.projeto.repositories.UsuarioRepository;
+
 @Controller
 @RequestMapping("perfil")
 public class PerfilController {
 
+	@Autowired
+	UsuarioRepository usuarioRepository;
+
 	@GetMapping()
-	public ModelAndView verDadosDoPerfil() {
+	public ModelAndView verDadosDoPerfil(@AuthenticationPrincipal User user) {
 		ModelAndView mv = new ModelAndView("perfil/detalhes");
+		Usuario u = usuarioRepository.findByEmail(user.getUsername());
+		mv.addObject("user", u);
 		return mv;
 	}
 
